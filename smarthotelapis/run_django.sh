8823d38b896d8f63f98a566bc506a6e12f1a28c1
QuyTruong <truong.4725212@gmail.com>
2026-01-03 17:37:10 +0700

    update .sh file

--- smarthotelapis/run_django.sh
+++ smarthotelapis/run_django.sh
@@ -1,3 +1,17 @@
 echo "=== Cài đặt thư viện ==="
 pip install -r requirements.txt
 
+echo "=== Tạo superuser ==="
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_EMAIL=truong.4725212@gmail.com
+export DJANGO_SUPERUSER_PASSWORD=admin123456
+
+python manage.py createsuperuser --no-input || echo "SuperUser đã tồn tại!"
+
+echo "=== Khởi tạo dữ liệu mẫu ==="
+python manage.py shell  <<EOF
+from smarthotel.models import Category
+c1, _ = Category.objects.get_or_create(name='vip')
+c2, _ = Category.objects.get_or_create(name='standard')
+
+EOF
